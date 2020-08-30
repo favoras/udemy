ed2a720234728bce978ea6d691ed617263c512b0
Edgaras <twixas99@gmail.com>
2020-08-30 19:20:20 +0300

    Day3 : More methods

--- src/com/company/day3/Main.java
+++ src/com/company/day3/Main.java
@@ -7,24 +7,58 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        System.out.println(calculateScore(false, score, levelCompleted, bonus));
-        // calculateScore(true, 800, 5, 100);
+//        int highScore = calculateScore(true, score, levelCompleted, bonus);
+//        System.out.println("Your final score was " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(true, score, levelCompleted, bonus);
+//        highScore = calculateScore(true, score, levelCompleted, bonus);
+//        calculateHighScorePosition(highScore);
 
+
+        displayHighScorePosition("Edgar", calculateHighScorePosition(1500));
+        displayHighScorePosition("Dave", calculateHighScorePosition(900));
+        displayHighScorePosition("Steve", calculateHighScorePosition(400));
+        displayHighScorePosition("Jack", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your Final Score: " + finalScore);
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + "  managed to get into position " + position + "  on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int highScore){
+
+        int position = 4; // base return
+
+        if(highScore >= 1000){
+            position = 1;
+        }else if (highScore >= 500){
+            position = 2;
+        }else if (highScore >= 100){
+            position = 3;
+        }
+        return position;
+
+
+
+//        if(highScore >= 1000){
+//            return 1;
+//        }else if (highScore >= 500){
+//            return 2;
+//        }else if (highScore >= 100){
+//            return 3;
+//        }
+//        return 4;
     }
 }
