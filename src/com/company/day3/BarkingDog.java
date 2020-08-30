66990b58084a8c5ec2aa0198f4d71237353f222e
Edgaras <twixas99@gmail.com>
2020-08-30 20:42:03 +0300

    Exercise BarkingDog done

---
+++ src/com/company/day3/BarkingDog.java
@@ -0,0 +1,23 @@
+package com.company.day3;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp (true, 1));
+        System.out.println(shouldWakeUp (false, 2));
+        System.out.println(shouldWakeUp (true, 8));
+        System.out.println(shouldWakeUp (true, -1));
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay <= 0 || hourOfDay > 23){
+            return false;
+        }else if(!barking){
+            return false;
+        }else if(hourOfDay < 8 || hourOfDay >= 22){
+            return true;
+        }
+        return false;
+    }
+}
