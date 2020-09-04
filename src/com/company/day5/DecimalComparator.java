321921f2aa5fa3c2e35dfcda11f6418740c9fe3c
Edgaras <twixas99@gmail.com>
2020-09-05 02:10:31 +0300

    Day 5: Decimal Comparator working in progress...

---
+++ src/com/company/day5/DecimalComparator.java
@@ -0,0 +1,12 @@
+package com.company.day5;
+
+public class DecimalComparator {
+    public static void main(String[] args) {
+        System.out.println("Working in progress...");
+    }
+
+
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+
+    }
+}
