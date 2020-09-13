157058992a184b6d6ac300aaea9b6f4bec3d7af3
Edgaras <twixas99@gmail.com>
2020-09-13 23:52:15 +0300

    Day 8: InheritanceChallenge done.

---
+++ src/com/company/day8/InheritanceChallenge.java
@@ -0,0 +1,127 @@
+package com.company.day8;
+
+public class InheritanceChallenge {
+    public static void main(String[] args) {
+
+
+        Outlander outlander = new Outlander(36);
+
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+
+    }
+}
+
+
+class Vehicle{
+
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction){
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees.");
+    }
+
+    public void move(int velocity, int direction){
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+    }
+
+    public void stop(){
+        this.currentVelocity = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+}
+
+class Car extends Vehicle{
+
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear){
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction){
+        System.out.println("Car.changeVelocity(): Velocity " + speed + " direction " + direction);
+        move(speed, direction);
+    }
+}
+
+class Outlander extends Car {
+
+    private int roadServiceMonths;
+
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate){
+        int newVelocity = getCurrentVelocity() + rate;
+
+        if(newVelocity == 0){
+            stop();
+        } else if (newVelocity > 0 && newVelocity <= 10){
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20){
+            changeGear(2);
+        } else if (newVelocity > 20 && newVelocity <= 30) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+
+        if(newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+
+    }
+
+}
